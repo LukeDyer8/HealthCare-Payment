5689a05d3c379e71489cb0e2ee20bac236017412
LukeDyer8 <lukedyert@gmail.com>
2023-04-11 23:28:19 -0400

    Provider New

---
+++ ProviderLogin2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="EN">
+  <head> 
+	<title>ProviderLogin2</title>
+	<link rel="stylesheet" type="text/css" href=".css" >
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  </head>
+  <body>
+    <p>
+	
+	<?php
+
+		
+		if ((isset($_POST["docUsername"]) && (!empty($_POST["docUsername"]))) &&
+			(isset($_POST["docPass"]) && (!empty($_POST["docPass"]))))
+		{	
+
+			$docUsername = htmlspecialchars($_POST["docUsername"]);
+			$docPass = htmlspecialchars($_POST["docPass"]);
+
+		if((preg_match("/^admin$/", $docUsername)) &&
+			(preg_match("/^admin$/", $docPass)))
+		{
+	?>		
+			Login Complete
+			Click <a href="Provider.html">  Here </a> to Continue to Provider Page.
+	
+		<?php 
+		}
+		else{
+
+		?>
+			Invalid Username and Password <br>
+			Please <a href="ProviderLogin.html"> Go Back </a> and Re-enter Your Username and Password.
+
+		<?php
+		}
+
+		}
+		else{
+
+		?>
+			Login Filled Out Incorrectly <br>
+			Please <a href="ProviderLogin.html"> Go Back </a> and Complete All Fileds.
+
+			<?php
+		}
+		?>
+		</p>
+	</body>
+</html>
---
+++ ProviderPresciprion.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="EN">
+<head>
+	<title> Refill Requests </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href=".css">
+</head>
+<body>
+<?php
+	//$db = mysqli_connect("studentdb-maria.gl.umbc.edu","ldyer2","ldyer2","ldyer2");
+
+
+
+
+		#$num_rows = mysqli_num_rows($select);
+
+
+		#if($num_rows != 0){
+
+			#for all the rows as returned by the query, go through each row
+			#and use the mysql_fetch_array function to return an array of the next row
+			#field values can be obtained by subscripting the returned aray with the column names
+?>
+<h2> All Entries: </h2>
+						<table border="1">
+					<tr>
+						<th> Title </th>
+						<th> Content </th>
+						<th> Emotion </th>
+						<th> Tags </th>
+					</tr>
+
+<?php
+
+ /* for($row_num = 0; $row_num < $num_rows; $row_num++){
+	print("<tr>");
+	$row_array = mysqli_fetch_array($select);
+	print("<td>$row_array[title]</td>");
+	print("<td>$row_array[content_text]</td>");
+	print("<td>$row_array[userFeeling]</td>");
+	print("<td>$row_array[userTags]</td>");
+	print("</tr>");		
+}
+}
+
+
+	print('</table>');
+	print('<br>');
+	print('<a href="https://swe.umbc.edu/~ldyer2/is448/ChapterSQL/postCreate.html"> Create Post </a>');
+	
+	} else {
+		echo "Please go back and fill out all information";
+	}
+	*/
+
+?>
+</body>
+</html>
